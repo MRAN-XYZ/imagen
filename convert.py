6ce3d05613e9b8960d1cf51e2f715b85db630311
MRAN-XYZ <kurniwatih91@gmail.com>
2025-08-25 14:48:03 +0700

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,12 +1,16 @@
 import tensorflow as tf
 
-# Replace 'model.keras' with your actual file path
+# Load your model
 model = tf.keras.models.load_model('logimagen.keras')
 
-# Convert the model.
+# Set up the converter with Select TF Ops enabled
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
+converter.target_spec.supported_ops = [
+    tf.lite.OpsSet.TFLITE_BUILTINS,
+    tf.lite.OpsSet.SELECT_TF_OPS,
+]
 tflite_model = converter.convert()
 
-# Save the model.
+# Save the converted model
 with open('model.tflite', 'wb') as f:
-  f.write(tflite_model)
+    f.write(tflite_model)
