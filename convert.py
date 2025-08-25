22ce438d03640611318182e12310959e8e1f9677
MRAN-XYZ <kurniwatih91@gmail.com>
2025-08-25 14:37:33 +0700

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 # Replace 'model.keras' with your actual file path
-model = keras.models.load_model('logimagen.keras')
+model = tf.keras.models.load_model('logimagen.keras')
 
 # Convert the model.
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
